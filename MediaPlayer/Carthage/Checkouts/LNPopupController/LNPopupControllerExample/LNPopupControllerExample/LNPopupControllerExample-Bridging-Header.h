7f8e6e6b11351b3b7bcd63c34529ef6e0618bfd4
Unknown <artembobrov3059@gmail.com>
2018-02-27 02:25:55 +0200

    extra code deleted

--- MediaPlayer/Carthage/Checkouts/LNPopupController/LNPopupControllerExample/LNPopupControllerExample/LNPopupControllerExample-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-@import LNPopupController;
-#import "LoremIpsum.h"
-#import "RandomColors.h"
-#import "SettingsTableViewController.h"
